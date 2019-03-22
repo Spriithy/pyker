2ec92160f01c65bd764025e4a0e9723b5e60b543
Spriithy <theo.dano@gmail.com>
2019-03-22 17:07:53 +0100

    Pretty message printer

--- client/Pinger.py
+++ client/Pinger.py
@@ -2,14 +2,17 @@ import time
 from threading import Thread
 from UserClass import user as user
 
-class Pinger(Thread):
 
+class Pinger(Thread):
     def __init__(self):
         Thread.__init__(self)
 
     def run(self):
-        while(True):
-            print(user.pull_Message())
-            time.sleep(0.1) #temps en sec
+        while (True):
+            for message in user.pull_Message():
+                print('%s <%s> %s' % (message['date'], message['from'],
+                                      message['content']))
+            time.sleep(0.1)  #temps en sec
 
-pinger=Pinger()
+
+pinger = Pinger()
