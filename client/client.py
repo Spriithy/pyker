1dec2b81f9ce6be61d498a5cebc96fc31697fec4
Spriithy <theo.dano@gmail.com>
2019-03-22 17:59:16 +0100

    init curses

--- client/client.py
+++ client/client.py
@@ -1,19 +1,23 @@
 import sys
 import os
 import InterfaceClass as itc
-#import threading
 from ApiCalls import instance_Server as api
 from UserClass import user as user
 from Pinger import pinger as pinger
+import curses
+from curses import wrapper
 
 
-it=itc.Interface
-itc.clear()
-it.standard_Print("Welcome to Pyker BB ")
-user.IDs_= it.set_Addr_Serv(sys.argv[1])
-user.ping_Serv()
+def main(stdscr):
+    stdscr.clear()
 
-pinger.start()
-pinger.join()
-input("___END___")
-exit(0)
+    # This raises ZeroDivisionError when i == 10.
+    for i in range(1, 9):
+        v = i - 10
+        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10 / v))
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+wrapper(main)
